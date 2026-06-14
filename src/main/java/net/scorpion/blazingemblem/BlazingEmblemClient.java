f3b2e22883bbbe893d17ba2bf85dccf4c466534c
Sean Harley <seanharley@macbookpro.mynetworksettings.com>
2026-06-14 15:11:41 -0400

    Initial commit

---
+++ src/main/java/net/scorpion/blazingemblem/BlazingEmblemClient.java
@@ -0,0 +1,10 @@
+package net.scorpion.blazingemblem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlazingEmblemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
